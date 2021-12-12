5294df1db183a624007ca6e10d6640bc77ca4db8
sheima-ayadi <80485125+sheima-ayadi@users.noreply.github.com>
2021-12-12 22:18:25 +0100

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -7,6 +7,40 @@ public class Server extends UnicastRemoteObject implements MyInterface {
 
     public Server() throws RemoteException {}
 
+    public String reverse(String s) {
+        
+        String rev="";
+        int n=s.length()-1;
+        for (int i=n; i>=0; i--)
+        {
+         rev=rev+ s.charAt(i); 
+         
+        }
+        return ans ;
+    }
+
+    public Character minChar(String s){
+        char min = 'z';
+
+        for (int i=0; i<s.length()-1; i++)   
+            if (s.charAt(i) < min)
+                min = s.charAt(i);  
+        
+        return min;         
+    }
+
+    public String caseChanger(String s){
+        
+        StringBuffer s1 =new StringBuffer(s); 
+         
+        for (int i=0; i<s.length(); i++)
+            if (Character.isLowerCase(s.charAt(i)))
+            s1.setCharAt(i, Character.toUpperCase(s.charAt(i)));
+            else   s1.setCharAt(i, Character.toLowerCase(s.charAt(i)));
+
+        return s1.toString();
+    }
+
     public static void main(String[] args) throws RemoteException, MalformedURLException {
         Server server = new Server();
 
